a18f847ab8fc599678227078f8904d2f6055d58a
bradley.velle <bradley.velle@student.howest.be>
2025-02-09 19:13:01 +0100

    Create blips for garages in config.lua

---
+++ client/main.lua
@@ -0,0 +1,32 @@
+-- Finds the middle of the zone and creates a blip there
+local function FindMiddleOfZone(zone)
+    local x, y, z = 0.0, 0.0, 0.0
+    for i, v in pairs(zone) do
+        x = x + v.x
+        y = y + v.y
+        z = z + v.z
+    end
+    return vector3(x / #zone, y / #zone, z / #zone)
+end
+
+local function CreateBlip(zone, blipName)
+    local blipCoords = FindMiddleOfZone(zone)
+    local blip = AddBlipForCoord(blipCoords.x, blipCoords.y, blipCoords.z)
+    SetBlipSprite(blip, Config.BlipNumber)
+    SetBlipDisplay(blip, 4)
+    SetBlipScale(blip, 0.6)
+    SetBlipColour(blip, Config.BlipColor)
+    SetBlipAsShortRange(blip, true)
+
+    BeginTextCommandSetBlipName("STRING")
+    if (Config.UniqueBlips) then
+        AddTextComponentString(blipName)
+    else
+        AddTextComponentString("Public Parking")
+    end
+    EndTextCommandSetBlipName(blip)
+end
+
+for i, v in pairs(Config.Garages) do
+    CreateBlip(v.takeVehicleZone, v.blipName)
+end
