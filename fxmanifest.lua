e4a7ad4eb887e63a4ef38ba2ead0cb5260e4d1f0
bradley.velle <bradley.velle@student.howest.be>
2025-02-09 11:31:19 +0100

    Add initial files and config file from qb-garages

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games { 'gta5' }
+lua54 'yes'
+
+name 'bradley-garages'
+author 'https://velleb.com'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config.lua',
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+     'server/main.lua'
+}
